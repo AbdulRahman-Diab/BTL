335323a751279a8414660fa693769a9e8b96a117
AbdulRahman-Diab <32856671+AbdulRahman-Diab@users.noreply.github.com>
2018-10-06 15:36:45 +0200

    Update Code.js

--- js/Code.js
+++ js/Code.js
@@ -1,33 +1,37 @@
-
 var LIndex = 0, RIndex = 0, db = firebase.firestore();
 //$("#leftNum").append("<li> 11111111111 </li>");
 var LeftNum = { Num: LIndex };
-db.collection(count).add(LeftNum);
+db.collection("count").add(LeftNum);
 
 var RightNum = { Num: RIndex };
-db.collection(count).add(RightNum);
+db.collection("count").add(RightNum);
 
 
 
 function LeftCountup() {
     LIndex++;
     var id0 = tasks.docs[0].id;
-    db.collection(count).doc(id0).set({ Num: LIndex });
+    db.collection("count").doc(id0).set({ Num: LIndex });
 
 }
 function LeftCountdown() {
     LIndex--;
     var id0 = tasks.docs[0].id;
-    db.collection(count).doc(id0).set({ Num: LIndex });
+    db.collection("count").doc(id0).set({ Num: LIndex });
 }
 
 function RightCountup() {
     RIndex++;
     var id1 = tasks.docs[1].id;
-    db.collection(count).doc(id1).set({ Num: RightNum });
+    db.collection("count").doc(id1).set({ Num: RightNum });
 }
 function RightCountdown() {
     RIndex--;
     var id1 = tasks.docs[1].id;
-    db.collection(count).doc(id1).set({ Num: RightNum });
+    db.collection("count").doc(id1).set({ Num: RightNum });
+}
+
+function handelUpdate(){
+    $("#leftNum").html(LeftNum.docs[0].data().Num);
+    $("#rightNum").html(RightNum.docs[1].data().Num);
 }
